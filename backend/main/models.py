8c51acc466e3b7fa78ffa73ae4108d238cca4773
neysanfoo <neysanfoo@gmail.com>
2023-01-16 23:07:34 +0800

    hello world endpoint backend

--- backend/main/models.py
+++ backend/main/models.py
@@ -1,3 +1,9 @@
 from django.db import models
 
 # Create your models here.
+
+class Hello(models.Model):
+    message = models.CharField(max_length=255)
+
+    def __str__(self):
+        return self.message
--- backend/main/views.py
+++ backend/main/views.py
@@ -1,3 +1,10 @@
-from django.shortcuts import render
+from rest_framework.views import APIView
+from rest_framework.response import Response
+from rest_framework import generics
+from rest_framework import permissions
+from .models import Hello
 
-# Create your views here.
+class HelloView(generics.ListAPIView):
+    def get(self, request):
+        message = Hello.objects.all()
+        return Response({"message": message[0].message})
