e8cbdd5394bf2da974fa0e4ac67a3f839ccf2b41
neysanfoo <neysanfoo@gmail.com>
2023-01-16 16:08:50 +0800

    set up backend and frontend

---
+++ frontend/src/components/Header.js
@@ -0,0 +1,24 @@
+import {Link} from 'react-router-dom'
+
+function Header() {
+  return (
+    <nav className="navbar navbar-expand-lg navbar-light ">
+      <div className="container">
+        <Link className="navbar-brand" to="/">KiasuKondos</Link>
+        <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNavAltMarkup" aria-controls="navbarNavAltMarkup" aria-expanded="false" aria-label="Toggle navigation">
+          <span className="navbar-toggler-icon"></span>
+        </button>
+        <div className="collapse navbar-collapse" id="navbarNavAltMarkup">
+          <div className="navbar-nav ms-auto">
+            <Link className="nav-link" to="/">Home</Link>
+            <a className="nav-link" href="#">Temp</a>
+            <a className="nav-link" href="#">Temp</a>
+          </div>
+        </div>
+      </div>
+    </nav>
+  );
+}
+
+export default Header;
+
