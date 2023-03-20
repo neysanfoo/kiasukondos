e993b2cf9f376681c7857baeefdb3da71819dd28
neysanfoo <neysanfoo@gmail.com>
2023-03-20 15:47:45 +0800

    a

--- frontend/src/components/Header.js
+++ frontend/src/components/Header.js
@@ -59,7 +59,7 @@ function Header() {
   return (
     <nav className="navbar navbar-expand-lg navbar-light ">
       <div className="container">
-        <a className="navbar-brand" href="/">KiasuKondos</a>
+        <a className="navbar-brand" href={auth ? "/homes" : "/"}>KiasuKondos</a>
         <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNavAltMarkup" aria-controls="navbarNavAltMarkup" aria-expanded="false" aria-label="Toggle navigation">
           <span className="navbar-toggler-icon"></span>
         </button>
